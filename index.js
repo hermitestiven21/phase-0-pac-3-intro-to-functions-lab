869fa67e97e5903cb6a3340e6eff03df0f1ab4f6
hermite <stivenhermite@gmail.com>
2023-03-17 02:49:35 -0400

    creates myfile.md

---
+++ index.js
@@ -0,0 +1,31 @@
+function shout(string) {
+    "Hello!".toUpperCase
+    return string.toUpperCase();
+    //
+}
+function whisper(string) {
+    "HELLO!".toLowerCase
+    return string.toLowerCase();
+
+}
+
+function logShout(string) {
+    console.log("hello".toUpperCase())
+}
+logShout()  
+
+function logWhisper(string) {
+    console.log("HELLO".toLowerCase())
+}
+logWhisper() 
+
+function sayHiToHeadphonedRoommate(string) {
+   if (string === string.toLowerCase())
+   return 'I can\'t hear you!';
+   else if (string === string.toUpperCase()) {
+    return 'YES INDEED!';
+   }
+   console.log ("Lets have dinner together!");
+   if (string = string)
+   return 'I would love to!';
+}
